4edd8749fab853991655f81b5e75c0e0877d9a86
rajurao <rajurao1107@gmail.com>
2025-05-16 16:48:39 +0530

    commit

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post("/api/contact", (req, res) => {
       secure: false,
       auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        pass: process.env.SMTP_PASS, 
       },
     });
 
