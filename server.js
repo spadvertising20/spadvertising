a4ffaab66fe177e6dc3adee4085402efdfdce818
rajurao <rajurao1107@gmail.com>
2025-05-16 18:25:19 +0530

    commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "Uploads")));
 
 // Create uploads folder if it doesn't exist
 const uploadPath = path.join(__dirname, "Uploads");
